22374e5910a2dffb4cdbf817193500a5bda45467
camj10 <113634102+camj10@users.noreply.github.com>
2022-10-31 22:50:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const wordContainer = document.getElementById('wordContainer');
-const startButton = document.getElementById('startButton');
-const usedLettersElement = document.getElementById('usedLetters');
+const contLetra = document.getElementById('contLetra'); 
+const PalabrasUsadasElement = document.getElementById('PalabrasUsadas');
+const inicio = document.getElementById('botonInicio');
 
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 ctx.canvas.width  = 0;
 ctx.canvas.height = 0;
 
-const bodyParts = [
+const ahorcado = [//Un array para ir dibujando las partes del cuerpo "ahorcado"
     [4,2,1,1],
     [4,3,1,2],
     [3,5,1,1],
@@ -16,99 +16,99 @@ const bodyParts = [
     [5,3,1,1]
 ];
 
-let selectedWord;
-let usedLetters;
-let mistakes;
-let hits;
+let palabraAdiv;
+let PalabrasUsadas; 
+let LetrasIncorrectas;
+let LetrasCorrectas;
 
-const addLetter = letter => {
-    const letterElement = document.createElement('span');
-    letterElement.innerHTML = letter.toUpperCase();
-    usedLettersElement.appendChild(letterElement);
+const agregarLetra = letter => {
+    const letraAnalizar = document.createElement('span');
+    letraAnalizar.innerHTML = letter.toUpperCase();
+    PalabrasUsadasElement.appendChild(letraAnalizar);
 }
 
-const addBodyPart = bodyPart => {
-    ctx.fillStyle = '#fff';
+const agregarAhorcado = bodyPart => {
+    ctx.fillStyle = '#f30798';
     ctx.fillRect(...bodyPart);
 };
 
-const wrongLetter = () => {
-    addBodyPart(bodyParts[mistakes]);
-    mistakes++;
-    if(mistakes === bodyParts.length) endGame();
+const IngresoIncorrecto = () => {
+    agregarAhorcado(ahorcado[LetrasIncorrectas]);
+    LetrasIncorrectas++;
+    if(LetrasIncorrectas === 5) FinDeJuego();
 }
 
-const endGame = () => {
+const FinDeJuego = () => {
     document.removeEventListener('keydown', letterEvent);
-    startButton.style.display = 'block';
+    inicio.style.display = 'block';
 }
 
-const correctLetter = letter => {
-    const { children } =  wordContainer;
+const IngresoCorrecto = letter => {
+    const { children } =  contLetra;
     for(let i = 0; i < children.length; i++) {
         if(children[i].innerHTML === letter) {
             children[i].classList.toggle('hidden');
-            hits++;
+            LetrasCorrectas++;
         }
     }
-    if(hits === selectedWord.length) endGame();
+    if(LetrasCorrectas === palabraAdiv.length){ FinDeJuego();} 
 }
 
-const letterInput = letter => {
-    if(selectedWord.includes(letter)) {
-        correctLetter(letter);
+const letterInput = letter => {//
+    if(palabraAdiv.includes(letter)) {
+        IngresoCorrecto(letter);
     } else {
-        wrongLetter();
+        IngresoIncorrecto();
     }
-    addLetter(letter);
-    usedLetters.push(letter);
+    agregarLetra(letter);
+    PalabrasUsadas.push(letter);
 };
 
 const letterEvent = event => {
     let newLetter = event.key.toUpperCase();
-    if(newLetter.match(/^[a-zñ]$/i) && !usedLetters.includes(newLetter)) {
+    if(newLetter.match(/^[a-zñ]$/i) && !PalabrasUsadas.includes(newLetter)) {
         letterInput(newLetter);
     };
 };
 
 const drawWord = () => {
-    selectedWord.forEach(letter => {
-        const letterElement = document.createElement('span');
-        letterElement.innerHTML = letter.toUpperCase();
-        letterElement.classList.add('letter');
-        letterElement.classList.add('hidden');
-        wordContainer.appendChild(letterElement);
+    palabraAdiv.forEach(letter => { 
+        const letraAnalizar = document.createElement('span');
+        letraAnalizar.innerHTML = letter.toUpperCase(); 
+        letraAnalizar.classList.add('letter'); 
+        letraAnalizar.classList.add('hidden');
+        contLetra.appendChild(letraAnalizar);
     });
 };
 
 const selectRandomWord = () => {
     let word = words[Math.floor((Math.random() * words.length))].toUpperCase();
-    selectedWord = word.split('');
+    palabraAdiv = word.split('');
 };
 
-const drawHangMan = () => {
+const drawHangMan = () => { //Dibujar al ahorcado
     ctx.canvas.width  = 120;
     ctx.canvas.height = 160;
     ctx.scale(20, 20);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = '#d95d39';
-    ctx.fillRect(0, 7, 4, 1);
+    ctx.clearRect(0, 0, canvas.width, canvas.height); //Borrar todo lo que haya
+    ctx.fillStyle = '#f00410'; 
+    ctx.fillRect(0, 7, 4, 1); 
     ctx.fillRect(1, 0, 1, 8);
     ctx.fillRect(2, 0, 3, 1);
     ctx.fillRect(4, 1, 1, 1);
 };
 
-const startGame = () => {
-    usedLetters = [];
-    mistakes = 0;
-    hits = 0;
-    wordContainer.innerHTML = '';
-    usedLettersElement.innerHTML = '';
-    startButton.style.display = 'none';
-    drawHangMan();
+const InicioDeJuego = () => {//Para empezar el juego: Limpiamos todas las variables del juego
+    PalabrasUsadas = [];
+    LetrasIncorrectas = 0;
+    LetrasCorrectas = 0;
+    contLetra.innerHTML = '';
+    PalabrasUsadasElement.innerHTML = '';
+    inicio.style.display = 'none'; 
+    drawHangMan(); 
     selectRandomWord();
     drawWord();
     document.addEventListener('keydown', letterEvent);
 };
 
-startButton.addEventListener('click', startGame);
+inicio.addEventListener('click', InicioDeJuego);
